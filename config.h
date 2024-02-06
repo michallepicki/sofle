9b19c19134eef816452a1864d303164cbfe52873
Michał Łępicki <michallepicki@users.noreply.github.com>
2024-02-06 21:28:46 +0100

    better way to flip encoder direction

--- config.h
+++ config.h
@@ -1,6 +1,4 @@
 #pragma once
 
-/* The way how "handedness" is decided (which half is which),
-see https://docs.qmk.fm/#/feature_split_keyboard?id=setting-handedness
-for more options.
-*/
+#define ENCODER_DIRECTION_FLIP
+// #define ENCODER_RESOLUTION 2
