19c82b530f1d6ac353edcd9e448a3c1a8faf9cce
Michał Łępicki <michallepicki@users.noreply.github.com>
2025-09-15 13:33:01 +0200

    Add Bspc Tab Enter and ESC to SYM layer

--- keymap.c
+++ keymap.c
@@ -52,11 +52,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /*
  * SYM
  * ,-----------------------------------------.                    ,-----------------------------------------.
- * |  `~  |  1!  |  2@  |  3#  |  4$  |  5%  |                    |  6^  |  7&  |  8*  |  9(  |  0)  |      |
+ * |  `~  |  1!  |  2@  |  3#  |  4$  |  5%  |                    |  6^  |  7&  |  8*  |  9(  |  0)  | Bspc |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      |      |      |      |      |      |                    |      |      |  -_  |  =+  |      |      |
+ * | Tab  |      |      |      |      |      |                    |      |      |  -_  |  =+  |      |Enter |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      |      |      |      |      |      |                    |      |  \|  |  [{  |  ]}  |  ;:  |  '"  |
+ * | ESC  |      |      |      |      |      |                    |      |  \|  |  [{  |  ]}  |  ;:  |  '"  |
  * |------+------+------+------+------+------|-------.    ,-------|------+------+------+------+------+------|
  * |LShift|      |      |      |      |      |       |    |       |      |      |  ,<  |  .>  |  /?  |RShift|
  * `-----------------------------------------/-------/     \-------\----------------------------------------'
@@ -67,11 +67,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_SYM] = LAYOUT(
 
-  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                          KC_6,    KC_7,    KC_8,    KC_9,   KC_0,    XXXXXXX,
+  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                          KC_6,    KC_7,    KC_8,    KC_9,   KC_0,    KC_BSPC,
 
-  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                       XXXXXXX, XXXXXXX, KC_MINS, KC_EQL, XXXXXXX, XXXXXXX,
+  KC_TAB,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                       XXXXXXX, XXXXXXX, KC_MINS, KC_EQL, XXXXXXX, KC_ENT,
 
-  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                       XXXXXXX, KC_BSLS, KC_LBRC, KC_RBRC, KC_SCLN, KC_QUOT,
+  KC_ESC,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                       XXXXXXX, KC_BSLS, KC_LBRC, KC_RBRC, KC_SCLN, KC_QUOT,
 
   KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX, KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
 
